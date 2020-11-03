c5be51d798c827f194ccf72cc9aae7381839b905
Varun Jain <varun18june@gmail.com>
2020-11-03 15:17:46 +0530

    change enviorment

--- src/environment.ts
+++ src/environment.ts
@@ -1,8 +1,5 @@
 enum Environments {
-    local_environment = 'local',
-    dev_environment = 'dev',
-    prod_environment = 'prod',
-    qa_environment = 'qa'
+    local_environment = 'local'
 }
 
 class Environment {
@@ -13,27 +10,11 @@ class Environment {
     }
 
     getPort(): Number {
-        if (this.environment === Environments.prod_environment) {
-            return 8081;
-        } else if (this.environment === Environments.dev_environment) {
-            return 8082;
-        } else if (this.environment === Environments.qa_environment) {
-            return 8083;
-        } else {
-            return 3000;
-        }
+        return 3000;
     }
 
     getDBName(): String {
-        if (this.environment === Environments.prod_environment) {
-            return 'db_test_project_prod';
-        } else if (this.environment === Environments.dev_environment) {
-            return 'db_test_project_dev';
-        } else if (this.environment === Environments.qa_environment) {
-            return 'db_test_project_qa';
-        } else {
-            return 'db_test_project_local';
-        }
+      return 'db_test';
     }
 }
 
