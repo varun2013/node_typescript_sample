7d1545d6d9ccfbf834f355721617a064c1debff5
Varun Jain <varun18june@gmail.com>
2020-11-03 16:44:46 +0530

    changes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,34 @@
 import app from "./config/app";
-import env from './environment'
+import env from './environment';
+
+import * as cors from 'cors';
+import * as express from "express";
+
+//get router
+var router = express.Router();
+
+//options for cors midddleware
+const options: cors.CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token',
+  ],
+  // credentials: true,
+  // methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  // origin: API_URL,
+  // preflightContinue: false,
+};
+
+//use cors middleware
+router.use(cors(options));
+
+//add your routes
+
+//enable pre-flight
+router.options('*', cors(options));
 
 const PORT = env.getPort();
 
