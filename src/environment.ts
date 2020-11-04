cfe0c488256e531be8bce842793b9ba2759ddfdd
Varun Jain <varun18june@gmail.com>
2020-11-04 11:06:01 +0530

    added conviroment

---
+++ src/environment.ts
@@ -0,0 +1,33 @@
+enum Environments {
+    local_environment = 'local'
+}
+
+class Environment {
+    private environment: String;
+
+    constructor(environment: String) {
+        this.environment = environment;
+    }
+
+    getPort(): Number {
+        return 5000;
+    }
+
+    getDBName(): String {
+      return 'db_test';
+    }
+
+    getAppUrl(): String {
+      return 'http://localhost:3002';
+    }
+
+    getMailUserName(): String {
+      return "azizkanwal10@gmail.com";
+    }
+
+    getMailPassword(): String {
+      return "Talentelgia!@#";
+    }
+}
+
+export default new Environment(Environments.local_environment);
